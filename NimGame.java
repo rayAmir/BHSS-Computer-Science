cf8807cf3686b65174710d7e6136f1701099293d
rayAmir <61857112+rayAmir@users.noreply.github.com>
2020-03-07 11:51:01 -0500

    Add files via upload

---
+++ NimGame.java
@@ -0,0 +1,30 @@
+/*
+* Date: March 7, 2020
+* Name: Rayyan Amir
+* Teacher: Mr. Ho
+* Description: Club Fair Assignment
+*/
+
+//P1: Start & initilize Code
+
+import java.util.Scanner;
+import java.util.Random;
+class NimGame{
+  public static void main(String[] args){
+    int total_items = 0;
+    int turn = 0;
+    
+//P2: Create Name System
+    Scanner myScanner = new Scanner (System.in);
+    System.out.println("Welcome to Nim Game");
+    System.out.println("User 1, enter your name:");
+    String user_one = myScanner.nextLine();
+    System.out.println("User 2, enter your name:");
+    String user_two = myScanner.nextLine();
+    
+    
+    
+    
+    
+  }
+}
