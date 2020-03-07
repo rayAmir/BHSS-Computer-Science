d013cc52effa65476cdb6e92579fe68b099a6bbb
rayAmir <61857112+rayAmir@users.noreply.github.com>
2020-03-07 12:04:40 -0500

    Add files via upload

--- NimGame.java
+++ NimGame.java
@@ -6,7 +6,6 @@
 */
 
 //P1: Start & initilize Code
-
 import java.util.Scanner;
 import java.util.Random;
 class NimGame{
@@ -22,9 +21,17 @@ class NimGame{
     System.out.println("User 2, enter your name:");
     String user_two = myScanner.nextLine();
     
+//P3: Create Pile Randomizer
+    Random rand = new Random(); 
+    int pile_a = rand.nextInt(10) + 2;
+    int pile_b = rand.nextInt(10) + 2;
+    int pile_c = rand.nextInt(10) + 2;
     
-    
-    
+//P4: Create the total items running counter and pile item counter
+    int item_a = pile_a;
+    int item_b = pile_b;
+    int item_c = pile_c;
+    total_items = pile_a + pile_b + pile_c;
     
   }
 }
