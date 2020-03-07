4ef343ef6bcc0f2a5139bf18984b6e0dccc12478
rayAmir <61857112+rayAmir@users.noreply.github.com>
2020-03-07 13:00:47 -0500

    Add files via upload

--- NimGame.java
+++ NimGame.java
@@ -43,17 +43,33 @@ class NimGame{
     if(turn %2 == 0){
       System.out.println(user_one + "," + " Choose a pile:");
       String reader = myScanner.nextLine();
-      // Scenerio 1: user 1 picks pile a and removes
+      // Scenerio 1: user 1 picks pile a and removes a #
       if (reader.equals("A")||reader.equals("a")) {
         System.out.println("How many to remove from pile A");
         String pile_1 = myScanner.nextLine();
         int pile_num1 = Integer.parseInt(pile_1);
         pile_a = pile_a - pile_num1;
+        turn++;
+        break;
       }
-      
-      
-      
-     
+      // Scenerio 2: User 1 picks pile a and removes a #
+      else if (reader.equals("B")||reader.equals("b")){
+        System.out.println("How many to remove from pile B");
+        String pile_2 = myScanner.nextLine();
+        int pile_num2 = Integer.parseInt(pile_2);
+        pile_b = pile_b - pile_num2; 
+        turn++;
+        break;
+      }
+      // Scenerio 3: User 1 picks pile 
+      else if (reader.equals("C")||reader.equals("c")){
+        System.out.println("How many to remove from pile C");
+        String pile_3 = myScanner.nextLine();
+        int pile_num3 = Integer.parseInt(pile_3);
+        pile_c = pile_c - pile_num3;
+        turn++;
+        break;
+      }     
     }
     
    }
